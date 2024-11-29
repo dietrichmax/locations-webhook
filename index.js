04d5011cd6bb1a729288b4cc6cf9133f89cb596e
Max Dietrich <mail@mxd.codes>
2024-11-29 08:27:16 +0100

    express

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const { Pool } = require('pg')
+
+const pool = new Pool()
+
+async function insertData(body) {
+  try {
+    const res = await pool.query(
+      "INSERT INTO locations (lat, lon, acc, alt, batt, bs, tst, vac, vel, conn, topic, inregions, ssid, bssid) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)",
+      [body.lat, body.lon, body.acc, body.alt, body.batt, body.bs, body.tst, body.vac, body.vel, body.conn, body.topic, body.inregions, body.ssid, body.bssid]
+    );
+    console.log(`Added location ${body.lat}, ${body.lon}`);
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+async function getCoordinates() {
+  const res = await pool.query(
+    "SELECT lat, lon FROM locations ORDER BY id DESC LIMIT 1"
+  );
+  return res
+}
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+
+app.get("/", async (req, res, next) => {
+
+    if (req.method === "POST") {
+        req.on('data', (chunk) => {
+         body.push(chunk);
+         }).on('end', () => {
+           body = JSON.parse(Buffer.concat(body).toString());
+           insertData(body)
+        })
+    } else if (req.method === "GET") {
+        try {
+            res.json(await getCoordinates());
+        } catch (err) {
+            console.error(`Error while getting coordinates `, err.message);
+            next(err);
+        }
+        coordinates = getCoordinates()
+        res.json();
+    }
+
+
+    
+        /*const coords = getCoordinates().then(res => {
+        const coords = res.rows[0]
+        return coords
+        })*/
+        
+
+});
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
