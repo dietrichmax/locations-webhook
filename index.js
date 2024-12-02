c6df6d89540c938987a09fe3af91853fa693b60a
Max Dietrich <mail@mxd.codes>
2024-12-02 10:35:31 +0100

    asd

--- index.js
+++ index.js
@@ -6,15 +6,11 @@ const { Pool } = require('pg')
 const pool = new Pool()   
 
 async function insertData(body) {
-  try {
-    await pool.query(
-      "INSERT INTO locations (lat, lon, acc, alt, batt, bs, tst, vac, vel, conn, topic, inregions, ssid, bssid) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)",
-      [body.lat, body.lon, body.acc, body.alt, body.batt, body.bs, body.tst, body.vac, body.vel, body.conn, body.topic, body.inregions, body.ssid, body.bssid]
-    );
-    console.log(`Added location ${body.lat}, ${body.lon}`);
-  } catch (error) {
-    console.error(error)
-  }
+  await pool.query(
+    "INSERT INTO locations (lat, lon, acc, alt, batt, bs, tst, vac, vel, conn, topic, inregions, ssid, bssid) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)",
+    [body.lat, body.lon, body.acc, body.alt, body.batt, body.bs, body.tst, body.vac, body.vel, body.conn, body.topic, body.inregions, body.ssid, body.bssid]
+  );
+  console.log(`Added location ${body.lat}, ${body.lon}`);
 }
 
 async function getCoordinates() {
