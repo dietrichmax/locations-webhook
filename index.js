9e2d145c4b72e68147cf16e608f8a46255cc46cd
Max Dietrich <mail@mxd.codes>
2024-12-14 20:08:38 +0100

    duplicate check

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ const { Pool} = require('pg')
 const pool = new Pool()
 
 async function insertData(body) {
-  const isDuplicate = await pool.query(
+  const isDuplicateSelect = await pool.query(
     `SELECT lat, lon FROM locations WHERE lat = ${body.lat} AND lon = ${body.lon}`
   );
+  const isDuplicate = isDuplicateSelect.rows > 0 ? True : false
+  console.log(isDuplicate)
+
   if (!isDuplicate) {
     const res = await pool.query(
       "INSERT INTO locations (lat, lon, acc, alt, batt, bs, tst, vac, vel, conn, topic, inregions, ssid, bssid) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)",
       [body.lat, body.lon, body.acc, body.alt, body.batt, body.bs, body.tst, body.vac, body.vel, body.conn, body.topic, body.inregions, body.ssid, body.bssid]
     );
-    console.log(`Added location ${body.lat}, ${body.lon}`);
+    console.log(`Added location lat: ${body.lat}, lon: ${body.lon}`);
   } else {
     console.log("is duplicate")
   }
@@ -41,7 +44,7 @@ server.on("request", async (request, response) => {
     body.push(chunk);
     }).on('end', () => {
       body = JSON.parse(Buffer.concat(body).toString());
-      console.log(body)
+      console.log("Requestbody" + body)
       if (body.lon && body.lat) {
         insertData(body)
       } else {
