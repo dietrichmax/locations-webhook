e96989186661db92a03d662fab1267e40a944980
Max Dietrich <mail@mxd.codes>
2026-01-07 17:26:02 +0100

    as

--- post_test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "https://geodata.mxd.codes/tracking/locations?api_key=hntrmfJjwTPlJbrTaCW0sLZr6iP2XZIt"
-myobj = {"lat": 21, "lon": 2}
-
-x = requests.post(url, json = myobj)
-
-print(x.text)
